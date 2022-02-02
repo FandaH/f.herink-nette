2942135983ee41414c76988adef9648a696d534f
Pavel Tatar <tatar@ssemi.cz>
2022-02-02 14:55:54 +0100

    new

--- app/Presenters/BasePresenter.php
+++ app/Presenters/BasePresenter.php
@@ -10,28 +10,5 @@ use Elasticsearch\Client;
 class BasePresenter extends \Nette\Application\UI\Presenter
 {
 
-    /**
-     * @var ElasticSearch @inject
-     */
-    public $elasticSearch;
-
-    /**
-     * @var DatabaseConnection @inject
-     */
-    public $database;
-
-    /**
-     * @var Client
-     */
-    public $elasticClient;
-
-
-    public function startup()
-    {
-        $this->elasticClient = $this->elasticSearch->getClient();
-
-        $this->elasticClient->connect();
-        die;
-    }
 
 }
