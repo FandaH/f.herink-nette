2709653c9f1af4ca5639eebbf848c19de40f7d5c
tatarp <Veronika)>
2022-04-05 18:09:22 +0200

    template

--- app/Presenters/BasePresenter.php
+++ app/Presenters/BasePresenter.php
@@ -10,5 +10,17 @@ use Elasticsearch\Client;
 class BasePresenter extends \Nette\Application\UI\Presenter
 {
 
+    /**
+     * @var \App\Models\Services\DatabaseConnection
+     * @inject
+     */
+public $database;
+
+    public function actionDefault(){
+
+$data = $this->database->getPdo()->query('SELECT * FROM pokus')->fetchAll();
+dump($data);
+die;
+    }
 
 }
